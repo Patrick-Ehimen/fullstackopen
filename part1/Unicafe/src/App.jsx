e74389338001ee4efc37b1c2fa472278595c3986
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-09-27 14:09:15 +0100

    Update App.jsx

--- part1/Unicafe/src/App.jsx
+++ part1/Unicafe/src/App.jsx
@@ -6,24 +6,50 @@ const Button = (props) => {
   return <button onClick={props.feedback}> {props.text}</button>;
 };
 
+const Statistics = (props) => {};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [average, setAverage] = useState(0);
+  const [percentageGood, setPercentageGood] = useState(0);
 
   const heading = "give feedback";
 
   const goodFeedback = () => {
-    setGood(good + 1);
+    const updatedGood = good + 1;
+    setGood(updatedGood);
+    updateTotalAndAverage(updatedGood, bad, neutral);
+
+    const updatedTotal = updatedGood + neutral + bad;
+    setPercentageGood((updatedGood / updatedTotal) * 100);
   };
 
   const neutralFeedback = () => {
-    setNeutral(neutral + 1);
+    const updatedNeutral = neutral + 1;
+    setNeutral(updatedNeutral);
+    updateTotalAndAverage(good, updatedNeutral, bad);
+
+    const updatedTotal = updatedNeutral + good + bad;
+    setPercentageGood((good / updatedTotal) * 100);
   };
 
   const badFeedback = () => {
-    setBad(bad + 1);
+    const updatedBad = bad + 1;
+    setBad(updatedBad);
+    updateTotalAndAverage(good, neutral, updatedBad);
+
+    const updatedTotal = updatedBad + good + neutral;
+    setPercentageGood((good / updatedTotal) * 100);
+  };
+
+  const updateTotalAndAverage = (good, neutral, bad) => {
+    const updatedTotal = good + neutral + bad;
+    setTotal(updatedTotal);
+    setAverage(updatedTotal ? (good - bad) / updatedTotal : 0);
   };
 
   return (
@@ -38,6 +64,10 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+
+      <p>all {total} </p>
+      <p>average {average} </p>
+      <p>positive {percentageGood} %</p>
     </div>
   );
 };
