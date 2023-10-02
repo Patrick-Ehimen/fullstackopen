4b20ad0dbf484df61a09a4fa2df5fc79d8732ce9
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-02 22:54:11 +0100

    Update PersonList.jsx

--- part2/phonebook/src/components/PersonList.jsx
+++ part2/phonebook/src/components/PersonList.jsx
@@ -1,7 +1,6 @@
 const PersonList = ({ persons }) => {
   return (
     <div>
-      <h2>Numbers</h2>
       {persons.map((person, index) => (
         <p key={index}>
           {person.name} {person.number}
