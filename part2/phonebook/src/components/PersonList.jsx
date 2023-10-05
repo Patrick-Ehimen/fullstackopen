bf6e745eedbe96faa01d8c66e7275d5002aff197
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-05 16:28:43 +0100

    Update PersonList.jsx

--- part2/phonebook/src/components/PersonList.jsx
+++ part2/phonebook/src/components/PersonList.jsx
@@ -1,12 +1,31 @@
+import personService from "../services/personService";
+
 const PersonList = ({ persons }) => {
+  const handleDelete = (id, name) => {
+    if (window.confirm(`Do you really want to delete ${name}?`)) {
+      personService
+        .deletePerson(id)
+        .then(() => {
+          // Update the local state after successful deletion
+          setns(persons.filter((person) => person.id !== id));
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   return (
-    <div>
-      {persons.map((person, index) => (
-        <p key={index}>
+    <ul>
+      {persons.map((person) => (
+        <li key={person.id}>
           {person.name} {person.number}
-        </p>
+          <button onClick={() => handleDelete(person.id, person.name)}>
+            Delete
+          </button>
+        </li>
       ))}
-    </div>
+    </ul>
   );
 };
 
