b6373d91b8069b6ffca8e90300421d79599de5a8
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-18 12:35:14 +0100

    Update App.jsx

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+
+const SearchFilter = ({ searchTerm, handleSearchChange }) => {
+  return (
+    <div>
+      Search Filter: <input value={searchTerm} onChange={handleSearchChange} />
+    </div>
+  );
+};
+
+const PersonForm = ({
+  newName,
+  newNumber,
+  handleNameChange,
+  handleNumberChange,
+  addPerson,
+}) => {
+  return (
+    <form onSubmit={addPerson}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  );
+};
+
+const PersonList = ({ persons }) => {
+  return (
+    <div>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => (
+        <p key={index}>
+          {person.name} {person.number}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-1234567" },
+    { name: "Ada Lovelace", number: "39-44-5323523" },
+    { name: "Dan Abramov", number: "12-43-234345" },
+    { name: "Mary Poppendieck", number: "39-23-6423122" },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    if (
+      !persons.some(
+        (person) => person.name.toLowerCase() === newName.toLowerCase()
+      )
+    ) {
+      const newPerson = { name: newName, number: newNumber };
+      setPersons(persons.concat(newPerson));
+    } else {
+      alert(`${newName} is already added to phonebook`);
+    }
+    setNewName("");
+    setNewNumber("");
+  };
+
+  const filteredPersons = persons.filter((person) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <SearchFilter
+        searchTerm={searchTerm}
+        handleSearchChange={handleSearchChange}
+      />
+      <h2>Add a new:</h2>
+      <PersonForm
+        newName={newName}
+        newNumber={newNumber}
+        handleNameChange={handleNameChange}
+        handleNumberChange={handleNumberChange}
+        addPerson={addPerson}
+      />
+      <PersonList persons={filteredPersons} />
+    </div>
+  );
+};
+
+export default App;
