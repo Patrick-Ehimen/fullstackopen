442b079841a6b9abee4449f94309d2574e8fd500
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-18 12:35:01 +0100

    Update App.jsx

--- part2/courseinfoApp/src/App.jsx
+++ part2/courseinfoApp/src/App.jsx
@@ -49,6 +49,7 @@ const App = () => {
 
   return (
     <div>
+      <h1>Web development curriculum</h1>
       {courses.map((course) => (
         <div key={course.id}>
           <Course course={course} />
