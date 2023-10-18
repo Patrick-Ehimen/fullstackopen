d4375806063e74b020c27eb6aa2c43207d02d407
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-10-18 19:30:29 +0100

    .

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,119 +1,138 @@
 import { useState, useEffect } from "react";
 
-import SearchFilter from "../components/SearchFilter";
-import PersonForm from "../components/PersonForm";
-import PersonList from "../components/PersonList";
-import Notification from "../components/Notification";
-import personService from "./personService";
+import Filter from "./components/Filter";
+import PersonForm from "./components/PersonForm";
+import Persons from "./components/Persons";
+import Notification from "./components/Notification";
+
+import entryService from "./services/entryService";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
-  const [searchTerm, setSearchTerm] = useState("");
-  const [errorMessage, setErrorMessage] = useState(null);
+  const [search, setSearch] = useState("");
+  const [notification, setNotification] = useState("");
+  const [showMsg, setShowMsg] = useState(false);
+
+  function submitForm(e) {
+    e.preventDefault();
+    if (!persons.some((person) => person.name === newName)) {
+      entryService
+        .createPerson({
+          name: newName,
+          number: newNumber,
+        })
+        .then((response) => {
+          setPersons([...persons, response]);
+          setNotification({
+            status: "success",
+            msg: `Success: You added ${newName} to the Phonebook!`,
+          });
+          setShowMsg(true);
+        })
+        .catch((err) => {
+          console.log(err);
+          setNotification({
+            status: "error",
+            msg: `Error: ${err.response.data.error}`,
+          });
+          setShowMsg(true);
+        });
+
+      setNewName("");
+      setNewNumber("");
+    } else {
+      if (
+        window.confirm(
+          `${newName} is already added, do you want to replace the old number with a new one?`
+        )
+      ) {
+        const personObj = persons.find((person) => person.name === newName);
+        const changedPerson = { ...personObj, number: newNumber };
+
+        entryService
+          .updatePerson(personObj._id, changedPerson)
+          .then((response) => {
+            setPersons((prevState) => {
+              return prevState.map((person) =>
+                person._id !== personObj._id ? person : response
+              );
+            });
+            setNotification({
+              status: "success",
+              msg: `Success: You edited ${newName}'s number in the Phonebook!`,
+            });
+            setShowMsg(true);
+          })
+          .catch((err) => {
+            setNotification({
+              status: "error",
+              msg: `Error: ${err}`,
+            });
+            setShowMsg(true);
+          });
+      }
+    }
+  }
+
+  function deletePerson(personName) {
+    if (window.confirm(`Are you sure you want to delete ${personName}`)) {
+      const personObj = persons.find((person) => person.name === personName);
+      entryService
+        .deletePerson(personObj._id, personObj)
+        .then((response) => {
+          setPersons((prevState) => {
+            setNotification({
+              status: "success",
+              msg: `Success: You deleted ${personObj.name} in the Phonebook!`,
+            });
+            setShowMsg(true);
+            return prevState.filter((person) => person._id !== personObj._id);
+          });
+        })
+        .catch((err) => {
+          setNotification({
+            status: "error",
+            msg: `Error: ${personObj.name} has already been deleted`,
+          });
+          setShowMsg(true);
+        });
+    }
+  }
 
   useEffect(() => {
-    personService.getAll().then((initialPerson) => {
-      setPersons(initialPerson);
+    entryService.getAll().then((response) => {
+      setPersons(response);
     });
   }, []);
 
-  const handleNameChange = (event) => {
-    setNewName(event.target.value);
-  };
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value);
-  };
-
-  const handleSearchChange = (event) => {
-    setSearchTerm(event.target.value);
-  };
-
-  const addPerson = (event) => {
-    event.preventDefault();
-    const existingPerson = persons.find(
-      (person) => person.name.toLowerCase() === newName.toLowerCase()
-    );
-
-    if (existingPerson) {
-      const confirmUpdate = window.confirm(
-        `${newName} is already added to the phonebook. Replace the old number with a new one?`
-      );
-
-      if (confirmUpdate) {
-        const updatedPerson = { ...existingPerson, number: newNumber };
-        personService
-          .update(existingPerson.id, updatedPerson)
-          .then((updatedPerson) => {
-            setPersons(
-              persons.map((person) =>
-                person.id === updatedPerson.id ? updatedPerson : person
-              )
-            );
-            setNewName("");
-            setNewNumber("");
-            setSuccessMessage(
-              `Number for ${updatedPerson.name} updated successfully.`
-            );
-            setTimeout(() => {
-              setSuccessMessage(null);
-            }, 5000);
-          })
-          .catch((error) => {
-            setErrorMessage(
-              `Failed to update the number for ${existingPerson.name}. Please try again.`
-            );
-            setTimeout(() => {
-              setErrorMessage(null);
-            }, 5000);
-          });
-      }
-    } else {
-      const newPerson = { name: newName, number: newNumber };
-      personService
-        .create(newPerson)
-        .then((createdPerson) => {
-          setPersons(persons.concat(createdPerson));
-          setNewName("");
-          setNewNumber("");
-          setSuccessMessage(`Person ${createdPerson.name} added successfully.`);
-          setTimeout(() => {
-            setSuccessMessage(null);
-          }, 5000);
-        })
-        .catch((error) => {
-          setErrorMessage(`Failed to add the person. Please try again.`);
-          setTimeout(() => {
-            setErrorMessage(null);
-          }, 5000);
-        });
+  useEffect(() => {
+    if (showMsg) {
+      const toRef = setTimeout(() => {
+        setShowMsg(false);
+        clearTimeout(toRef);
+      }, 3000);
     }
-  };
-
-  const filteredPersons = persons.filter((person) =>
-    person.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  }, [showMsg]);
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <Notification message={errorMessage} />
-      <SearchFilter
-        searchTerm={searchTerm}
-        handleSearchChange={handleSearchChange}
-      />
-      <h2>Add a new:</h2>
+      {showMsg && (
+        <Notification status={notification.status} message={notification.msg} />
+      )}
+      <Filter search={search} persons={persons} setSearch={setSearch} />
+      <h3>Add new</h3>
       <PersonForm
         newName={newName}
         newNumber={newNumber}
-        handleNameChange={handleNameChange}
-        handleNumberChange={handleNumberChange}
-        addPerson={addPerson}
+        submitForm={submitForm}
+        setNewName={setNewName}
+        setNewNumber={setNewNumber}
       />
-      <h2>Numbers</h2>
-      <PersonList persons={filteredPersons} setPersons={setPersons} />
+      <h3>Numbers</h3>
+      <Persons persons={persons} deletePerson={deletePerson} />
     </div>
   );
 };
---
+++ part2/phonebook/src/components/Content.jsx
@@ -0,0 +1,24 @@
+import Person from "./Persons";
+
+const Content = ({ persons, allPersons, deletePerson }) => {
+  console.log(persons.length);
+  if (persons.length === 0) {
+    return (
+      <ul>
+        {allPersons.map((person, i) => (
+          <Person key={i} person={person} deletePerson={deletePerson} />
+        ))}
+      </ul>
+    );
+  } else {
+    return (
+      <ul>
+        {persons.map((person, i) => (
+          <Person key={i} person={person} deletePerson={deletePerson} />
+        ))}
+      </ul>
+    );
+  }
+};
+
+export default Content;
